889f0ce6c0ecc5feedb5131dd6a5341eb3befb6a
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-03-27 10:03:30 +0700

    status kelulusan

--- ExerciseProsedural_115/ExerciseProsedural_115.cpp
+++ ExerciseProsedural_115/ExerciseProsedural_115.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
-using namespace std;
+using namespace std ;
 
 double rerata(double a, double b) {
-	return (a + b) / 2;
+    return (a + b) / 2;
 }
 
 string status(double rata) {
-	if (rata >= 70)
-		return "lulus";
-	else
-		return "gagal"
+    if (rata >= 70)
+        return "lulus";
+    else
+        return "gagal";
 }
 
 string status2(double rata, double nil) {
-	if (rata >= 70 || nil >= 80)
-		return "lulus";
-	else
-		return "gagal";
+    if (rata >= 70 || nil >= 80)
+    return "lulus";
+    else
+    return "gagal";
 }
 
-
+int main() {
+    double ni1M, ni1B;
+    cout << "masukan nilai matematika = ";
+    cin >> ni1M;
+    cout << "masukan nilai bahasa inggris = ";
+    cin >> ni1B;
+    cout << "status kelulusan = " << status(rerata(ni1M, ni1B));
+    cout << "\nstatus kelulusan ke 2 = " << status2
+    (rerata(ni1B, ni1M), ni1B);
+    return 0;
+}
 
